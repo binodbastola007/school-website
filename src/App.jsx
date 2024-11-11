19c66936157333f15613495e7b46e6731c858a16
binod bastola <bndbstl@gmail.com>
2024-11-11 10:33:12 +0545

    added project to github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import RegistrationForm from '../Components/RegistrationForm';
+
+const Register = () => {
+    return (
+        <>
+            <div>Registration Form</div>
+            <RegistrationForm />
+        </>
+
+
+.key
+
+    )
+}
+
+export default Register
