1fcad91b7e20a896d11a449cb0fc651501e23229
binod bastola <bndbstl@gmail.com>
2024-12-19 21:29:10 +0545

    added scrolltotop component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop';
 
 const Layout = ({ children }) => {
     return (
         <div className="layout">
-
+            <ScrollToTop />
                 <Navbar />
                 <main className='main-container'>{children}</main>
                 <Footer />
